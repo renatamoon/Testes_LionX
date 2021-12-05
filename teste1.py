683d6afabaf7276e50dcdb6e3629d78797d040e4
Renata Monteiro <r.cardoso.monteiro1@gmail.com>
2021-12-05 20:25:21 -0300

    final commit

---
+++ teste1.py
@@ -0,0 +1,32 @@
+"""1) Write a function that takes in a non-empty array (array_value) of distinct integers and an integer
+representing a target sum (target_sum). If any two numbers in the input array sum up to the target
+sum, the function should return them in an array, in any order. If no two numbers sum up to the
+target sum, the function should return an empty array.
+
+Inputs:
+array_value = [3, 5, -4, 8, 11, 1, -1, 6]
+target_sum = 10
+Outputs:
+It’s a list of numbers or empty list
+"""
+
+array_value = [3, 5, -4, 8, 11, 1, -1, 6]
+target_sum = 10
+
+
+def return_the_two_num_array(array_value, target_sum):
+    list = []
+    lengh = len(array_value) - 1
+    lengh_int = len(array_value)    
+    for index in range(lengh):
+        for num in range(index + 1, lengh_int):            
+            if array_value[index] + array_value[num] == target_sum:
+                list.append(array_value[index],)
+                list.append(array_value[num])
+                if len(list) == 0:
+                    print('OUTPUT: The list is empty')
+                else:
+                    print("OUTPUT: It's a list of numbers")
+    return list
+
+print(return_the_two_num_array(array_value, target_sum))
---
+++ teste2.py
@@ -0,0 +1,25 @@
+"""2) Given two non-empty arrays of integers (array_value and sequence), write a function that
+determines whether the second array is a subsequence of the first one.
+
+Inputs:
+array_value = [5, 1, 22, 25, 6, -1, 8, 10]
+sequence = [1, 6, -1, 10]
+Outputs:
+It’s a boolean value"""
+
+array_value = [5, 1, 22, 25, 6, -1, 8, 10]
+sequence = [1, 6, -1, 10]
+
+def validation_subsequence_array(array_value, sequence):
+    sequenc_cont = 0
+    lengh_sequence = len(sequence)
+    for i in array_value:
+        if sequenc_cont == lengh_sequence:
+            if i == sequence[sequenc_cont]:
+                sequenc_cont += 1
+        if sequenc_cont == lengh_sequence:
+            return True
+        else:
+            return False
+
+print(validation_subsequence_array(array_value, sequence))
